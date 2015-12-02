41e08ecd8452e1b8b80d9fae9dadc53d0fcb19a4
Andrew <andrew.popenko@forprogress.com.pl>
2015-12-02 13:19:10 +0100

    1

--- src/gardenstore/tests/SampleTest.java
+++ src/gardenstore/tests/SampleTest.java
@@ -2,6 +2,9 @@ package gardenstore.tests;
 
 
 import fp.forevo.manager.MasterScript;
+import fp.forevo.manager.TafException;
+import gardenstore.modules.HomePage;
+import gardenstore.modules.LoginPage;
 
 public class SampleTest extends MasterScript{
 
@@ -11,18 +14,24 @@ public class SampleTest extends MasterScript{
 	
 	private void run() {
 	
-		/*log.info("");
+		log.info("");
 		log.warning("", "");
 		HomePage homePage = new HomePage();
 		LoginPage loginPage = new LoginPage();
 		
 		
-		homePage.jOpenLoginPage();
-		loginPage.jLogin("tester@forprogress.com.pl", "12345678");
-		homePage.jChangeLanguage("EN");
-		homePage.jOpenContactPage();
+		try {
+			homePage.jOpenGardenStore();
+			loginPage.jLogin("tester@forprogress.com.pl", "12345678");
+			homePage.jChangeLanguage("EN");
+			homePage.jOpenContactPage();
+			
+			homePage.jLogout();
+		} catch (TafException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		homePage.jLogout();	*/
 
 	}
 
